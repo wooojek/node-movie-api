7b27baa5a9fe756d9f3d6160a83ba498f8331bfd
wooojek <yokomura123@gmail.com>
2018-08-12 23:48:56 +0200

    Added tests for POST /movies route

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -5,17 +5,10 @@ const { getMovieData } = require('../utils/utils');
 const { app } = require('../index');
 const { Movie } = require('../models/movie');
 
-const movies = [{
-    text: '1st test comment',
-    author: '1st test author'
-}, {
-    text: '2nd test comment',
-    author: '2nd test author'
-}];
-
-beforeEach((done) => {
-    Movie.remove({}).then(() => {
-        return await getMovieData('i', 'tt3896198');
+before((done) => {
+    Movie.remove({}).then(async () => {
+        const movieData = await getMovieData('i', 'tt1856101');
+        return Movie.insertMany([movieData]);
     }).then((() => done()));
 });
 
@@ -31,6 +24,100 @@ describe('POST /movies', () => {
             .expect((res) => {
                 expect(res.body.imdbID).toBe(id);
             })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Movie.find({
+                    imdbID: id
+                }).then((movies) => {
+                    expect(movies.length).toBe(1);
+                    expect(movies[0].imdbID).toBe(id);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should get additional movie data by title and save it to database', (done) => {
+        const title = 'Guardians of the Galaxy';
+        request(app)
+            .post('/movies')
+            .send({
+                title
+            })
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.Title).toBe(title);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Movie.find({
+                    Title: title
+                }).then((movies) => {
+                    expect(movies.length).toBe(1);
+                    expect(movies[0].Title).toBe(title);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not get additional data if movie is present in database', (done) => {
+        const id = 'tt1856101';
+        request(app)
+            .post('/movies')
+            .send({
+                id
+            })
+            .expect(422)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Movie.find({
+                    imdbID: id
+                }).then((movies) => {
+                    expect(movies.length).toBe(1);
+                    expect(movies[0].imdbID).toBe(id);
+                    done();
+                }).catch((e) => done(e));
+            });
+    });
+
+    it('should not get additional data if supplied with wrong body.data', (done) => {
+        const ida = 'tt1856101';
+        request(app)
+            .post('/movies')
+            .send({
+                ida
+            })
+            .expect(400)
             .end(done);
     });
+
+    it('should not save movie if there is none', (done) => {
+        const title = 'HappyLittleTrees';
+        request(app)
+            .post('/movies')
+            .send({
+                title
+            })
+            .expect(404)
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+
+                Movie.find({
+                    Title: title
+                }).then((movies) => {
+                    expect(movies.length).toBe(0);
+                    done();
+                }).catch((e) => done(e));
+            });
+    })
 });
