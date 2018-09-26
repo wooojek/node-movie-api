91849cf6b48ce30e3abbe337cdc8d3bedd00da6a
wooojek <yokomura123@gmail.com>
2018-09-26 22:16:40 +0200

    Added docs for API

---
+++ api_project.js
@@ -0,0 +1,1 @@
+define({  "title": "apiDoc for movie-api",  "name": "movie-api documentation",  "version": "0.0.1",  "description": "Movie API documentation",  "sampleUrl": false,  "defaultVersion": "0.0.0",  "apidoc": "0.3.0",  "generator": {    "name": "apidoc",    "time": "2018-09-26T20:15:51.746Z",    "url": "http://apidocjs.com",    "version": "0.17.6"  }});
